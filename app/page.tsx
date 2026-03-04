b9961eeb690450e1bbf03571bb22ba0887666afc
Charlene <charlene@whatthebleep.co.uk>
2026-03-04 10:51:44 +0800

    Initial commit from Charlene

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">
+        Hello! I am building my first AI app.
+      </h1>
+    </main>
+  )
+}
